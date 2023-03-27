45bde5d7f2501ddf44640fc3a4e87e0e666d8fc5
Your Name <your@email.com>
2023-03-27 18:35:06 +0545

    features product added in user from database

--- src/main/java/Service/productservice.java
+++ src/main/java/Service/productservice.java
@@ -6,13 +6,17 @@ package Service;
 
 import Databaseconnection.DBConnection;
 import Model.Product;
+import Model.user;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class productservice {
-    
-    public void addProduct(Product product){
-    String query = "insert into product(product_id, product_Name, product_Price, product_Image, product_Category) values(?,?,?,?,?);";
+public class Productservice {
+
+    public void addProduct(Product product) {
+        String query = "insert into product(product_id, product_Name, product_Price, product_Image, product_Category) values(?,?,?,?,?);";
 
         PreparedStatement preparedStatement = new DBConnection().getStatement(query);
         try {
@@ -25,9 +29,27 @@ public class productservice {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-}
-    
-    public void deleteProduct(Product product_id){
-        PreparedStatement.
+    }
+
+    public List<Product> getProductsByFeatures() {
+        Product product = null;
+        List<Product> products = new ArrayList<Product>();
+        String query = "select * from product where product_Category='features';";
+        PreparedStatement preparedStatement = new DBConnection().getStatement(query);
+        try {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                product = new Product();
+                product.setProduct_id(resultSet.getInt("product_id"));
+                product.setProduct_Image(resultSet.getString("product_Image"));
+                product.setProduct_Name(resultSet.getString("product_Name"));
+                product.setProduct_Price(resultSet.getString("product_Price"));
+                product.setProduct_category(resultSet.getString("product_Category"));
+                products.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return products;
     }
 }
--- src/main/java/controller/UserServlet.java
+++ src/main/java/controller/UserServlet.java
@@ -1,15 +1,18 @@
 package controller;
 
+import Model.Product;
 import Model.user;
+import Service.Productservice;
 import Service.userservice;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet(name = "UserServlet", urlPatterns = {"/UserServlet"})
 public class UserServlet extends HttpServlet {
@@ -61,13 +64,24 @@ public class UserServlet extends HttpServlet {
             userservice service = new userservice();
             System.out.println(service.getUser(email, password));
 
-            if (service.getUser(email, password)) {
-                RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
-                rd.forward(request, response);
-            } else {
+            //Validating the User in database
+            if (!service.getUser(email, password)) {
                 RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
                 rd.forward(request, response);
+
             }
+            HttpSession session = request.getSession();     //Creating session
+            session.setAttribute("email", email);
+            
+            Product product = new Product();
+            List<Product> featuresProduct = new Productservice().getProductsByFeatures();
+
+                        request.setAttribute("product", product);
+
+            request.setAttribute("featuresProduct", featuresProduct);
+
+            RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+            rd.forward(request, response);
 
         }
     }
